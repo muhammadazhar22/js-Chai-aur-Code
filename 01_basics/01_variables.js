ae6bccdec90a85ca4df192da33a1638115ab7cfd
muhammadazhar22 <41356674+muhammadazhar22@users.noreply.github.com>
2023-11-01 17:05:37 +0000

    editing devcontainer.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "azhar@google.com"
+var accountPassword ="1234"
+accountCity = "lahore"
+
+// accoundId 2 // nor allowed
+
+accountEmail = "az@az.com"
+accountPassword = "21212121"
+accountCity = "murree"
+let accountState;
+
+console.log(accountId)
+
+/*
+
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
