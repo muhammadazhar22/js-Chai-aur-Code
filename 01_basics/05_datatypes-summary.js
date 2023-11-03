4adcef43ae1177bddfe6f14a155195db5e1220cb
muhammadazhar22 <41356674+muhammadazhar22@users.noreply.github.com>
2023-11-03 15:54:54 +0000

    datatype summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,40 @@
+//  Primirive
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+// console.log(id)
+// console.log(anotherId)
+
+// const bigNumber = 3456543576654356754n
+
+
+
+
+
+
+// Reference (Non primitive) 
+
+// Array, Objects, Functions
+
+const heros = ["superman", "batman", "ironman"] 
+let myObj = {
+    name: "azhar",
+    age: 24,
+}
+
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+console.log(typeof id)
