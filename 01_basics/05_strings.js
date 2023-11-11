ff43a2143a8a8c49239ec7c51c9ad1a0e2a6e213
muhammadazhar22 <muhammadazhar9526@gmail.com>
2023-11-11 07:01:48 +0500

    string methods

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,31 +3,77 @@ const repoCount = 3
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt('2'));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.charAt('2'));
+// console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0,4);
-console.log(newString);
+// console.log(newString);
 
 const anotherString = gameName.slice(-8,4)
-console.log(anotherString);
+// console.log(anotherString);
 
 const newStringOne = "   hitesh   "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
 const url = "https:hitesh.com/hitesh%20choudary"
 
-console.log(url.replace('%20', '-'))
-console.log(url.includes('azhar'))
-console.log(gameName.split('-'))
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('azhar'))
+// console.log(gameName.split('-'))
+const secondName = "Hel lo-wor ld"
+const splitArr = secondName.split(' ')
+// console.log(splitArr.join(''))
+
+
+// console.log("Ha".repeat(5))
+
+
+ const paragraph = "I think Ruth's dog is cuter i than i your i dog!";
+
+// const searchTerm = 'i';
+// const indexOfFirst = paragraph.indexOf(searchTerm);
+
+// console.log(`The index of the first "${searchTerm}" is ${indexOfFirst}`);
+// // Expected output: "The index of the first "dog" is 15"
+
+// console.log(
+//   `The index of the second "${searchTerm}" is ${paragraph.indexOf(
+//     searchTerm,
+//     indexOfFirst + 20,
+//   )}`,
+// );
+
+// console.log(typeof(paragraph.indexOf()))
+
+// console.log(paragraph.charAt(0));
+
+// console.log(paragraph[0]);
+
+
+const a = "aga";
+const b = "aBCD";
+// if (a < b) {
+//   // true
+//   console.log(`${a} is less than ${b}`);
+// } else if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else {
+//   console.log(`${a} and ${b} are equal.`);
+// }
+
+
+// console.log(a.toUpperCase())
+// console.log(b.toLowerCase())
+
+console.log(paragraph.at(-1))
