5d5f46252628035a2fc6866f5faa0683253adca7
muhammadazhar22 <muhammadazhar9526@gmail.com>
2023-11-17 00:20:02 +0500

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -45,10 +45,45 @@ const user = [
 ]
 
 user[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
+// console.log(Object.keys(tinderUser));
 //console.log(Object.keys(regularUser.fullname));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
+const course = {
+    name: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+// const {courseInstructor} =  course
+const {courseInstructor : instructor} =  course
+// console.log(courseInstructor)
+console.log(instructor);
+
+
+// react
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "hitesh")
+
+
+// {
+//     "name": "js in hindi",
+//     "courseInstructor": "hitesh",
+//     "price": "free"
+
+// }
+
+[
+    {},
+    {},
+    {}
+]
